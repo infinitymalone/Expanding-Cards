269e2636fc4aaa2d127c43d2213007e485eacb98
Tushar rawat <65559868+infinitymalone@users.noreply.github.com>
2021-09-02 13:35:02 +0530

    Add files via upload

---
+++ expand.js
@@ -0,0 +1,23 @@
+const panels= document.querySelectorAll('.panel');
+panels.forEach((panel)=>
+{
+    panel.addEventListener('click',function(){
+        removeActiveClass();
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClass()
+{
+    panels.forEach(panel=>{
+        panel.classList.remove('active');
+    })
+}
+
+
+
+
+
+
+
+
